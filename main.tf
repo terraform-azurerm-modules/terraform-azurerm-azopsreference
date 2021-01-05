a741cacc5f6d524163cc84413ae5489ea22342f6
Matt White <matt.white@microsoft.com>
2021-01-05 11:36:30 +0000

    Implement new policyset parameter_values syntax. Required provider 2.33.0 or above. (#24)

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.20.0"
+      version = ">= 2.33.0"
     }
   }
   required_version = ">= 0.13"
